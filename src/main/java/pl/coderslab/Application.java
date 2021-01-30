843e26b7beee62a870b95053b3bf288b0344cc68
Makary01 <makary.bortnowski@gmail.com>
2021-01-30 20:09:11 +0100

    NO NIE JESTEM

--- src/main/java/pl/coderslab/Application.java
+++ src/main/java/pl/coderslab/Application.java
@@ -2,16 +2,9 @@ package pl.coderslab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @EnableJpaRepositories
